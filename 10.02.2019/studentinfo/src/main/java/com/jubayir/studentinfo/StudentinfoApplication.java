922bd58337874dc4901eefb845ebdb1b64653255
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-02-10 22:02:29 +0600

    Spring Practice10

---
+++ 10.02.2019/studentinfo/src/main/java/com/jubayir/studentinfo/StudentinfoApplication.java
@@ -0,0 +1,14 @@
+package com.jubayir.studentinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentinfoApplication.class, args);
+    }
+
+}
+
