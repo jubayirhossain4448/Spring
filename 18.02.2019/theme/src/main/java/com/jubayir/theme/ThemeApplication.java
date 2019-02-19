a729575f369716048f1e605f7ec89713b69155d2
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-02-19 06:23:09 +0600

    security With database own2

---
+++ 18.02.2019/theme/src/main/java/com/jubayir/theme/ThemeApplication.java
@@ -0,0 +1,13 @@
+package com.jubayir.theme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeApplication.class, args);
+    }
+
+}
