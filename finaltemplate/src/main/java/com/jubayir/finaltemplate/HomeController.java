c6674a70c58acc84fdbdb512549b69785ea858d3
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-02-25 23:06:21 +0600

    final

---
+++ finaltemplate/src/main/java/com/jubayir/finaltemplate/HomeController.java
@@ -0,0 +1,12 @@
+package com.jubayir.finaltemplate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/")
+    public String displayHome(){
+        return "layout";
+    }
+}
