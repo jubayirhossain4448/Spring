a87e3539815034e5e42e9be87e8874f92316f4b0
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-02-25 19:27:52 +0600

    final_templating

---
+++ finaltemplate/src/main/java/com/jubayir/finaltemplate/FinaltemplateApplication.java
@@ -0,0 +1,13 @@
+package com.jubayir.finaltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinaltemplateApplication.class, args);
+    }
+
+}
